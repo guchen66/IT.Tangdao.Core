02808cfe3fad2cf7eac555ce785dd6bdb23c7ada
liuxin <lxdy_523@163.com>
2025-11-22 23:59:22 +0800

    增加decimal类型和double类型生成器

--- IT.Tangdao.Core/Helpers/TangdaoDataFaker.cs
+++ IT.Tangdao.Core/Helpers/TangdaoDataFaker.cs
@@ -100,7 +100,15 @@ namespace IT.Tangdao.Core.Helpers
                 {
                     return FakeDataHelper.GenerateRandomString(fakeDataAttr.Length);
                 }
+                if ((property.PropertyType == typeof(float) || property.PropertyType == typeof(double)))
+                {
+                    return FakeDataHelper.GenerateDoubleUniqueId(fakeDataAttr.Min, fakeDataAttr.Max, fakeDataAttr.Point);
+                }
 
+                if (property.PropertyType == typeof(decimal))
+                {
+                    return FakeDataHelper.GenerateDecimalUniqueId(fakeDataAttr.Min, fakeDataAttr.Max, fakeDataAttr.Point);
+                }
                 // 2.3 第三优先级：DataType
                 if (property.PropertyType.IsEnum)
                 {
@@ -139,7 +147,9 @@ namespace IT.Tangdao.Core.Helpers
         {
             if (propertyType == typeof(string)) return FakeDataHelper.GenerateRandomString();
             if (propertyType == typeof(int)) return FakeDataHelper.GenerateUniqueId();
+            if (propertyType == typeof(double) || propertyType == typeof(float)) return FakeDataHelper.GenerateDoubleUniqueId();
             if (propertyType == typeof(long)) return (long)FakeDataHelper.GenerateUniqueId();
+            if (propertyType == typeof(decimal)) return (long)FakeDataHelper.GenerateDecimalUniqueId();
             if (propertyType == typeof(DateTime)) return FakeDataHelper.GenerateRandomDateTime();
             if (propertyType == typeof(bool)) return FakeDataHelper.GetRandomBoolean();
             if (propertyType.IsEnum) return FakeDataHelper.GetRandomEnumValue(propertyType);
